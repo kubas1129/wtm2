49f827bb1d401a36b9cf33c6bfba214a6a248838
macOSHighSierra <macoshighsierra@macOSHighSierras-Mac.local>
2019-11-10 06:11:06 -0800

    map worked

--- SplitViewWeatherApp/SearchVC.swift
+++ SplitViewWeatherApp/SearchVC.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class SearchVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
@@ -17,19 +19,83 @@ class SearchVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var addLocButton: UIButton!
+    @IBOutlet weak var locCityName: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
     
     var searchingCityName: String = ""
     var cities : [CityInfo] = []
     var selectCity : CityInfo = CityInfo(name: "", woeid: 0)
+    var localizationCity : CityInfo = CityInfo(name: "", woeid: 0)
+    
+    let locationManager = CLLocationManager()
+    let regionInMeters: Double = 10000
+    var previousLocation: CLLocation?
     
     let urlToSearch = "https://www.metaweather.com/api/location/search/?query="
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        checkLocationServices()
 
         // Do any additional setup after loading the view.
         tableView.delegate = self
         tableView.dataSource = self
+    }
+    
+    func checkLocationAuthorization() {
+        switch CLLocationManager.authorizationStatus() {
+        case .authorizedWhenInUse:
+            locCityName.text = "Szukanie..."
+            startTackingUserLocation()
+            break
+        case .denied:
+            // Show alert instructing them how to turn on permissions
+            break
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            // Show an alert letting them know what's up
+            break
+        case .authorizedAlways:
+            break
+        }
+    }
+    
+    func centerViewOnUserLocation() {
+        if let location = locationManager.location?.coordinate {
+            let region = MKCoordinateRegion.init(center: location, latitudinalMeters: regionInMeters, longitudinalMeters: regionInMeters)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func checkLocationServices() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocationManager()
+            checkLocationAuthorization()
+        } else {
+            // Show alert letting the user know they have to turn this on.
+        }
+    }
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func getCenterLocation(for mapView: MKMapView) -> CLLocation {
+        let latitude = mapView.centerCoordinate.latitude
+        let longitude = mapView.centerCoordinate.longitude
+        
+        return CLLocation(latitude: latitude, longitude: longitude)
+    }
+    
+    func startTackingUserLocation() {
+        mapView.showsUserLocation = true
+        centerViewOnUserLocation()
+        locationManager.startUpdatingLocation()
+        previousLocation = getCenterLocation(for: mapView)
     }
     
     
@@ -42,6 +108,10 @@ class SearchVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
         ///nothing here
     }
     
+    
+    @IBAction func onLocClick(_ sender: Any) {
+        selectCity = localizationCity
+    }
     
     
     func updateCities(){
@@ -84,6 +154,24 @@ class SearchVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
         
     }
     
+    func updateLocalizationCity(){
+        let locURL : URL? = URL(string: "https://www.metaweather.com/api/location/search/?lattlong=\(previousLocation?.coordinate.latitude ?? 0.0),\(previousLocation?.coordinate.longitude ?? 0.0)")
+        
+        if(locURL != nil)
+        {
+            URLSession.shared.dataTask(with: locURL!) {
+                data, resp, err in
+                
+                let dataJson = try? JSONSerialization.jsonObject(with: data!, options: []) as? [[String:Any]]
+                
+                var record = dataJson!![0]
+                
+                self.localizationCity = CityInfo(name: record["title"] as! String,
+                                                 woeid: record["woeid"] as! Int)
+                }.resume()
+        }
+    }
+    
     func updateTableView(){
         self.tableView.reloadData()
     }
@@ -112,6 +200,48 @@ class SearchVC: UIViewController,UITableViewDelegate, UITableViewDataSource {
         return cell
     }
     
-   
+}
 
+extension SearchVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        checkLocationAuthorization()
+    }
+}
+
+extension SearchVC: MKMapViewDelegate{
+    func mapView(_ mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        let center = getCenterLocation(for: mapView)
+        let geoCoder = CLGeocoder()
+        print("REGION CHANGED")
+        guard let previousLocation = self.previousLocation else { return }
+        
+        guard center.distance(from: previousLocation) > 50 else { return }
+        self.previousLocation = center
+        
+        geoCoder.reverseGeocodeLocation(center) { [weak self] (placemarks, error) in
+            guard let self = self else { return }
+            
+            if let _ = error {
+                print("ERROR")
+                return }
+            
+            guard let placemark = placemarks?.first else {
+                print("NO PLACEMAARK")
+                return }
+            
+            //let streetNumber = placemark.subThoroughfare ?? ""
+            //let streetName = placemark.thoroughfare ?? ""
+            let city = placemark.subAdministrativeArea ?? ""
+            let country = placemark.country ?? ""
+            
+            print("TU JEST OK")
+            
+            DispatchQueue.main.async {
+                self.updateLocalizationCity()
+                self.locCityName.text = "\(country), \(city)"
+                print("CITY: \(city)")
+            }
+        }
+        
+    }
 }
